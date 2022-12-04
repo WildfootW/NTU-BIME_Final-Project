e90ab57e38b2a66dc68501b038c7b0e0f32ceca7
WildfootW <wildfootw@wildfoo.tw>
2022-12-04 17:55:25 +0800

    ROS_PKG: init

---
+++ Software/ROS_PKG/bime_final/scripts/wheel_control.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyleft (ɔ) 2022 wildfootw <wildfootw@wildfoo.tw>
+#
+# Distributed under terms of the MIT license.
+
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+
+def wheel_control():
+    rospy.init_node('wheel_control')
+
+    rospy.Subscriber('chatter', String, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    wheel_control()
